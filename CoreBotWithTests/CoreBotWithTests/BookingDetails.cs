d23ee15a2db484551348eabba79b402c7fac1785
AlphaWolf6789 <quanbatman6789@gmail.com>
2022-06-18 15:48:09 +0700

    Hi

---
+++ CoreBotWithTests/CoreBotWithTests/BookingDetails.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio CoreBot v4.15.2
+
+
+namespace CoreBotWithTests
+{
+    public class BookingDetails
+    {
+        public string Error { get; set; }
+
+        public string Room { get; set; }
+
+        public string Computer { get; set; }
+    }
+}
---
+++ CoreBotWithTests/CoreBotWithTests/Dialogs/BookingDialog.cs
@@ -0,0 +1,126 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio CoreBot v4.15.2
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Microsoft.Recognizers.Text.DataTypes.TimexExpression;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoreBotWithTests.Dialogs
+{
+    public class BookingDialog : CancelAndHelpDialog
+    {
+        private const string ErrorStepMsgText = "Bạn gặp lỗi gì?";
+        private const string RoomStepMsgText = "Bạn đang ở phòng nào?";
+        private const string ComputerStepMsgText = "Số máy?";
+
+        public BookingDialog()
+            : base(nameof(BookingDialog))
+        {
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new ConfirmPrompt(nameof(ConfirmPrompt)));
+
+            var waterfallSteps = new WaterfallStep[]
+            {
+                    ErrorStepAsync,
+                    RoomStepAsync,
+                    ComputerStepAsync,
+                    ConfirmStepAsync,
+                    FinalStepAsync,
+            };
+
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallSteps));
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private static bool IsAmbiguous(string timex)
+        {
+            var timexProperty = new TimexProperty(timex);
+            return !timexProperty.Types.Contains(Constants.TimexTypes.Definite);
+        }
+
+        private async Task<DialogTurnResult> ErrorStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var bookingDetails = (BookingDetails)stepContext.Options;
+
+            if (bookingDetails.Error == null)
+            {
+                var promptMessage = MessageFactory.Text(ErrorStepMsgText, ErrorStepMsgText, InputHints.ExpectingInput);
+                return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
+            }
+
+            return await stepContext.NextAsync(bookingDetails.Error, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> RoomStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var bookingDetails = (BookingDetails)stepContext.Options;
+
+            bookingDetails.Error = (string)stepContext.Result;
+
+            if (bookingDetails.Room == null)
+            {
+                var promptMessage = MessageFactory.Text(RoomStepMsgText, RoomStepMsgText, InputHints.ExpectingInput);
+                return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
+            }
+
+            return await stepContext.NextAsync(bookingDetails.Room, cancellationToken);
+        }
+        private async Task<DialogTurnResult> ComputerStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var bookingDetails = (BookingDetails)stepContext.Options;
+            bookingDetails.Room = (string)stepContext.Result;
+
+            if (bookingDetails.Computer == null)
+            {
+                var promptMessage = MessageFactory.Text(ComputerStepMsgText, ComputerStepMsgText, InputHints.ExpectingInput);
+                return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
+            }
+
+            return await stepContext.NextAsync(bookingDetails.Computer, cancellationToken);
+        }
+        //private async Task<DialogTurnResult> TravelDateStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        //{
+        //    var bookingDetails = (BookingDetails)stepContext.Options;
+
+        //    bookingDetails.Room = (string)stepContext.Result;
+
+        //    if (bookingDetails.TravelDate == null || IsAmbiguous(bookingDetails.TravelDate))
+        //    {
+        //        return await stepContext.BeginDialogAsync(nameof(ComputerResolverDialog), bookingDetails.TravelDate, cancellationToken);
+        //    }
+
+        //    return await stepContext.NextAsync(bookingDetails.TravelDate, cancellationToken);
+        //}
+
+        private async Task<DialogTurnResult> ConfirmStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var bookingDetails = (BookingDetails)stepContext.Options;
+
+            bookingDetails.Computer = (string)stepContext.Result;
+
+            var messageText = $"Vui lòng xác nhận lại, Bạn gặp lỗi: {bookingDetails.Error} ở máy: {bookingDetails.Computer}, phòng: {bookingDetails.Room}. Bạn muốn gọi Kỹ thuật viên chứ?";
+            var promptMessage = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput);
+
+            return await stepContext.PromptAsync(nameof(ConfirmPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            if ((bool)stepContext.Result)
+            {
+                var bookingDetails = (BookingDetails)stepContext.Options;
+
+                return await stepContext.EndDialogAsync(bookingDetails, cancellationToken);
+            }
+
+            return await stepContext.EndDialogAsync(null, cancellationToken);
+        }
+    }
+}
